681b715e9bb15637fc4e72d67f67f70623a9c9a6
Justin Harr <justin@Justins-MBP.lan>
2018-09-20 19:48:15 -0400

    Proxy heroku

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true //To allow https from Heroku
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
