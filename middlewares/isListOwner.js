6c6c9002d89c8b0194d89064f9d58c5dd8d6f155
Justin Harr <justin@Justins-MBP.lan>
2019-02-24 16:49:04 -0500

    secure delete route

--- middlewares/isListOwner.js
+++ middlewares/isListOwner.js
@@ -1,5 +1,20 @@
-module.exports = (req, res, next) => {
+exports.variant_get = (req, res, next) => {
     const { formId } = req.query;
+
+    var ownsList = req.user.forms.some(function (form) {
+       
+        return form.equals(formId);
+    });
+
+    if (!formId || !ownsList ){
+        return res
+          .status(401)
+          .json({ success: false, message: "This contact list does not belong to you" });
+      }
+      next();
+}
+exports.variant_post = (req, res, next) => {
+    const { formId } = req.body;
 
     var ownsList = req.user.forms.some(function (form) {
        
--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -5,11 +5,11 @@ var form_controller = require("../controllers/formController");
 
 module.exports = app => {
   app.post("/forms/create", requireLogin, form_controller.create_form);
-  app.post("/forms/delete", requireLogin, form_controller.delete_form);
+  app.post("/forms/delete", requireLogin, isListOwner.variant_post, form_controller.delete_form);
 
   app.get("/forms/get", form_controller.get_form);
   app.post("/forms/respond", form_controller.submit_response);
-  app.get("/forms/responses", requireLogin, isListOwner, form_controller.fetch_responses);
+  app.get("/forms/responses", requireLogin, isListOwner.variant_get, form_controller.fetch_responses);
   app.get("/forms/all", requireLogin, form_controller.fetch_forms);
 
 };
